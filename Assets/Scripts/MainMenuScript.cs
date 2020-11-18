95864c83028de1d1951e1b33b971e9608ef246db
wyatthamby <69863331+wyatthamby@users.noreply.github.com>
2020-11-17 20:23:36 -0500

    Adding menu to project
    Adding the most recent iteration of the main menu - mostly made from UI elements. Options and credits to be filled out later

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Call to play
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Call to quit
+    public void QuitGame ()
+    {
+        // Debug to show this works
+        Debug.Log("Program Terminated");
+        Application.Quit();
+    }
+}
