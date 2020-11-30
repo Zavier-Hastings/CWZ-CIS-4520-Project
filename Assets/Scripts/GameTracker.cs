89ee7780ca0938ecce5383b61429c1927932b10e
Zavier-Hastings <69856460+Zavier-Hastings@users.noreply.github.com>
2020-11-29 19:49:30 -0500

    Fixed the turn counter, the round counter doesn't work because the object is destroyed when the scene reloads, so it never has the chance to save any variable to pull an integer from.

--- Assets/Scripts/GameTracker.cs
+++ Assets/Scripts/GameTracker.cs
@@ -2,26 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameTracker : MonoBehaviour
 {
     public Sprite Tracker;
-    public Text TurnCount;
-    public Text RoundCount;
+    public TMP_Text TurnCount;
+    public TMP_Text RoundCount;
+
+    public Canvas canvas;
 
     public int turn_count = 0;
-    public int round_count = 0;
+    public int round_count = 1;
 
-    public void Activate()
+    public void Activate() //!<Sets the sprite when called, and both counters down to their default value
     {
         this.GetComponent<SpriteRenderer>().sprite = Tracker;
-        this.TurnCount = Instantiate(TurnCount, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0), Quaternion.identity);
+
+        canvas = Instantiate(canvas, new Vector3(this.transform.position.x, this.transform.position.y, -1), Quaternion.identity);
+        canvas.GetComponent<Canvas>();
+        canvas.transform.SetParent(this.transform);
+
+        TurnCount.GetComponent<TMP_Text>();
+        TurnCount = Instantiate(TurnCount, new Vector3(-3.45f, -1.9f, -1), Quaternion.identity);
+        TurnCount.transform.SetParent(this.canvas.transform, false);
+
+        RoundCount.GetComponent<TMP_Text>();
+        RoundCount = Instantiate(RoundCount, new Vector3(6.9f, -1.9f, -1), Quaternion.identity);
+        RoundCount.transform.SetParent(this.canvas.transform, false);
+
+        TurnCount.text = turn_count.ToString();
+        RoundCount.text = round_count.ToString();
     }
+
 
     public void increment_turn()
     {
         turn_count++;
-        this.TurnCount.text = this.turn_count.ToString();
+        TurnCount.text = this.turn_count.ToString();
+    }
+
+    public void increment_round()
+    {
+        round_count++;
+        RoundCount.text = this.round_count.ToString();
     }
 
 }
